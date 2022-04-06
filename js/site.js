6738b138f7637ea6963aec1e3d4763d4128e5e74
Bruno Aviles <bruno.aviles@yahoo.com>
2022-04-06 11:37:11 -0500

    create app functionality

--- js/site.js
+++ js/site.js
@@ -1,4 +1,49 @@
-// Call Hello World
-function HelloWorld() {
-    alert("hello world");
+// get user input
+function getValues(){
+    document.getElementById("displayBox").classList.add("invisible");
+    
+    let userText = document.getElementById("userInput").value;
+    let onlyText = removeSpecialCharacters(userText);
+    // let validateText = verifyInput(onlyText);
+
+    verifyInput(onlyText);
+}
+
+// remove any special characters
+function removeSpecialCharacters(text){
+    let lettersOnly = "";
+    for(i = 0;i < text.length; i++){
+        // special characters
+        let specialChar = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~ ]/;
+
+        if(specialChar.test(text[i]) == true) {
+            lettersOnly += "";
+        } else {
+            lettersOnly += text[i];
+        }
+    }
+    return lettersOnly;
+}
+
+// verify if word, phrase, sequence is a palindrome
+function verifyInput(validate){
+    let reverse = "";
+    // arrange the text reading backwards
+    for(i = (validate.length - 1); i >= 0; i--){
+        reverse += validate[i];
+    }
+    // alert("word in reverse: " + reverse);
+    if(validate == reverse){
+        alertDisplay.innerHTML = "Success!";
+        msgDisplay.innerHTML = "Your Palindrome is:";
+        resultDisplay.innerHTML = `${validate} = ${reverse}`;
+    
+        displayBox.classList.remove("invisible");
+    } else {
+        alertDisplay.innerHTML = "Sorry...";
+        msgDisplay.innerHTML = "Your Input Is Not A Palindrome";
+        resultDisplay.innerHTML = `${validate} = ${reverse}`;
+
+        displayBox.classList.remove("invisible");
+    }
 }
